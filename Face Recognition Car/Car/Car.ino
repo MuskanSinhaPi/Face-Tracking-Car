b522dd889dde104d171f58a2d693b3bfd572ab1b
Muskan Sinha <97770929+MuskanSinhaPi@users.noreply.github.com>
2024-05-03 21:01:27 +0530

    Update Car.ino

--- Face Recognition Car/Car/Car.ino
+++ Face Recognition Car/Car/Car.ino
@@ -1,128 +1,115 @@
 #include <AFMotor.h>
 #include <SoftwareSerial.h>
-int data[1];
 
 AF_DCMotor left(1);
 AF_DCMotor right(3);
-//const int leftMotorPin1 = 2;
-//const int leftMotorPin2 = 3;
-//const int rightMotorPin1 = 4;
-//const int rightMotorPin2 = 5;
 
 //SoftwareSerial XBee(4,5); // RX, TX pins
-
-//int vspeed=100;    
-//int tspeed=255;
-//int tdelay=20;
+int direction;
 
 void setup()
 {
- Serial.begin(9600);
-// XBee.begin(9600);
- left.setSpeed(200);
- right.setSpeed(200);
- left.run(RELEASE);
- right.run(RELEASE);
-//
-//  pinMode(leftMotorPin1, OUTPUT);
-//  pinMode(leftMotorPin2, OUTPUT);
-//  pinMode(rightMotorPin1, OUTPUT);
-//  pinMode(rightMotorPin2, OUTPUT);
+  Serial.begin(9600);
+  // XBee.begin(9600);
+  left.setSpeed(200);
+  right.setSpeed(200);
+  left.run(RELEASE);
+  right.run(RELEASE);
 }
 
 void loop() {
-//  if (XBee.available()) { //receive
-    if (Serial.available()) { //receive
-      int direction = Serial.read();
-      processDirection(direction);
-  }
-}
+//    if (XBee.available()) { //receive
+  if (Serial.available()>0) { //receive
+    direction = Serial.parseInt();
+    Serial.println(direction);
+    }
 
-void processDirection(int direction) {
   switch (direction) {
     case 1:  // Back Left
-      moveBackLeft();
+      moveForward();
       break;
     case 2:  // Back
       moveBack();
       break;
     case 3:  // Back right
-      moveBackRight();
+      moveLeft();
       break;
     case 4:  // Left
-      moveLeft();
+      moveRight();
       break;
     case 5:  // Stay still
       stopMoving();
       break;
-    case 6:  // Right
-      moveRight();
-      break;
-    case 7:  // Front Left
-      moveForwardLeft();
-      break;
-    case 8:  // Forward
-      moveForward();
-      break;
-    case 9:  // Forward right
-      moveForwardRight();
-      break;
+//    case 6:  // Right
+//      
+//      break;
+//    case 7:  // Front Left
+//      moveForwardLeft();
+//      break;
+//    case 8:  // Forward
+//      
+//      break;
+//    case 9:  // Forward right
+//      moveForwardRight();
+//      break;
     default: // Unknown direction or stay still
       stopMoving();
       break;
   }
+
 }
 
+
 void moveForward() {
-  left.run(FORWARD);
-  right.run(FORWARD);
+  left.run(BACKWARD);
+  right.run(BACKWARD);
   left.setSpeed(200);
   right.setSpeed(200);
 }
 
 void moveBack() {
-  left.run(BACKWARD);
-  right.run(BACKWARD);
+  left.run(FORWARD);
+  right.run(FORWARD);
   left.setSpeed(200);
   right.setSpeed(200);
 }
 
 void moveLeft() {
   left.run(RELEASE);
-  right.run(FORWARD);
+  right.run(BACKWARD);
   right.setSpeed(200);
 }
 
 void moveRight() {
-  left.run(FORWARD);
+  left.run(BACKWARD);
   right.run(RELEASE);
   left.setSpeed(200);
 }
 
 void moveForwardLeft() {
-  left.run(FORWARD);
-  right.run(FORWARD);
+  left.run(BACKWARD);
+  right.run(BACKWARD);
   left.setSpeed(200);
   right.setSpeed(400);
 }
 
 void moveForwardRight() {
-  left.run(FORWARD);
-  right.run(FORWARD);
+  left.run(BACKWARD);
+  right.run(BACKWARD);
   left.setSpeed(400);
   right.setSpeed(200);
 }
 
 void moveBackLeft() {
-  left.run(BACKWARD);
-  right.run(BACKWARD);
+  left.run(FORWARD);
+  right.run(FORWARD);
   left.setSpeed(200);
   right.setSpeed(400);
 }
 
 void moveBackRight() {
-  left.run(BACKWARD);
-  right.run(BACKWARD);
+  left.run(FORWARD);
+  right.run(FORWARD);
   left.setSpeed(400);
   right.setSpeed(200);
 }
@@ -131,51 +118,3 @@ void stopMoving() {
   left.run(RELEASE);
   right.run(RELEASE);
 }
-//   
-//  if(c=='f')
-//  {
-//  forward(); 
-//  }
-//
-//  else if(c=='l')
-//  {
-//  left(); 
-//  }
-// 
-//  else if(c=='r')
-//   { 
-//  right(); 
-//  }
-//  
-//  else if(c=='s')
-//   {
-//  none(); 
-//  }
-//}
-//
-//void forward() {
-//digitalWrite(m1, HIGH);
-//digitalWrite(m2, HIGH);
-//}
-////
-////void back() {
-////digitalWrite(leftMotorBackward,  HIGH);
-////digitalWrite(rightMotorBackward, HIGH);
-////digitalWrite(leftMotorForward,  LOW);
-////digitalWrite(rightMotorForward, LOW);
-////}
-//
-//void left() {
-//digitalWrite(m1, LOW);
-//digitalWrite(m2, HIGH);
-//}
-//
-//void  right() {
-//digitalWrite(m1, HIGH);
-//digitalWrite(m2,  LOW);
-//}
-//
-//void none() {
-//digitalWrite(m1, LOW);
-//digitalWrite(m2, LOW);
-//}
